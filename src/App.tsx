03b1d8dfd44c43ffc8db4b571c65f6ae052c0c3c
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-04 14:48:05 +0800

    add: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div className="parent w-full h-screen p-4">
+      <div className="max-w-[640px] mx-auto">
+        <Header />
+        <main></main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
