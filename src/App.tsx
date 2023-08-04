aca452d94066593fa8b318715f48e13cae311af2
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-04 15:33:42 +0800

    refactor: css and layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import WorkInProgress from "./components/WorkInProgress";
 
 function App() {
   return (
-    <div className="parent w-full h-screen p-4">
-      <div className="max-w-[640px] mx-auto">
-        <Header />
-        <WorkInProgress />
-      </div>
+    <div className="p-5 max-w-[640px] mx-auto">
+      <Header />
+      <WorkInProgress />
     </div>
   );
 }
