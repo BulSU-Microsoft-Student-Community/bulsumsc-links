fee696e83e670c9c3a56abe76aae0365d4e452dd
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-08-08 23:52:43 +0800

    add: `EventItem` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import EventItem from "./components/EventItem";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import WorkInProgress from "./components/WorkInProgress";
@@ -7,6 +8,18 @@ function App() {
   return (
     <div className="p-5 max-w-[640px] mx-auto">
       <Header />
+      <div className="flex flex-col my-3">
+        <h3 className="text-center text-white font-semibold text-md">
+          Upcoming Events 📆
+        </h3>
+        <EventItem
+          link="https://www.facebook.com/bulsu.officialmsc/posts/290388803498900"
+          date="08-29"
+          subheading="Pitching Competition"
+        >
+          Ctrl+Alt+Innovate: Unleashing Tech Brilliance
+        </EventItem>
+      </div>
       <WorkInProgress />
       <Footer />
     </div>
---
+++ src/components/EventItem.tsx
@@ -0,0 +1,32 @@
+type EventItemProps = {
+  link: string;
+  subheading?: string;
+  date?: string;
+  children?: React.ReactNode;
+};
+const EventItem: React.FC<EventItemProps> = ({
+  link,
+  children,
+  subheading,
+  date,
+}) => {
+  return (
+    <a href={link} target="_blank">
+      <div className="bg-white px-6 py-3 rounded-lg shadow border-2 border-green-500">
+        <p className="text-center font-semibold text-ms-blue">
+          {children || "Empty Event Item"}
+        </p>
+        {subheading || date ? (
+          <div className="flex flex-row justify-between items-center text-sm text-gray-500 mt-2 pt-2 border-t">
+            <span className="">{subheading}</span>
+            <span className="">{date}</span>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </a>
+  );
+};
+
+export default EventItem;
